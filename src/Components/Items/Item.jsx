aaa54c91b7847c9c6af19f3ca8b6c5a499bbde49
wasifafaque8 <afaqueplayer4@gmail.com>
2024-08-06 21:39:13 +0530

    6/8/24

--- src/Components/Items/Item.jsx
+++ src/Components/Items/Item.jsx
@@ -8,7 +8,7 @@ const Item = (props) => {
   return(
 
     <div className='item'>
-      <Link to={`/product/${props.id}`}><img src={props.image} alt="" /> </Link> 
+      <Link to={`/product/${props.id}`}><img  onClick={window.scrollTo(0,0)} src={props.image} alt="" /> </Link> 
       
    
         <p>{props.name}</p>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
 
 
 
@@ -11,6 +12,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
 
     const[menu,setMenu]=useState("shop");
+    const{getTotalCartItems} =useContext(ShopContext);
 
 
 
@@ -34,7 +36,7 @@ const Navbar = () => {
       <div className='nav-logo-cart'>
         <Link to="/login" ><button>Login</button></Link>
         <Link to="/cart" ><img src={cart_icon} alt="/"></img></Link>
-        <div className='nav-cart-count'>0</div>
+        <div className='nav-cart-count'>{getTotalCartItems()}</div>
 
       </div>
 
